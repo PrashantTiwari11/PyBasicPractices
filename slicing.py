6f93d98041acffc0d4ca0eddecf58bc12e8fb02d
Prashant <pt867987@gmail.com>
2026-02-25 11:37:19 +0530

    Performed slicing concept .

---
+++ slicing.py
@@ -0,0 +1,6 @@
+x = str(input("Enter you text for slice : "))
+si = int(input("Enter your start index: "))
+ei = int(input("Enter your end index: "))
+result = x [si:ei]
+print("Original text is " + x)
+print ("Slicied text is "+ result)
